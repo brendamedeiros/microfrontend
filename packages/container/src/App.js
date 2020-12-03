5ba9a1909c013865351c1aaa67aa024e05438fb7
brendamedeiros <brenda.medeiros@outlook.com>
2020-12-03 12:29:10 -0300

    Remove console.log

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
-console.log(MarketingApp);
-
 export default () => {
     return <div>
             <h1>Hi there!</h1>
